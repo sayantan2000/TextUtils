613f5af73aa16cdf3f90e1ab0b82fe3472a597aa
sayantansk13 <sayantansk13@gmail.com>
2021-12-14 15:01:31 +0530

    added encrypt text & decrypter

--- README.md
+++ README.md
@@ -9,7 +9,14 @@ It is a simple django project or website in which we can Analyze text.
 3)New Line Remove<br>
 4)Extra Spaces Remover<br>
 5)Numbers Remover
+6)Encrypt Text
+7)Decrypt The Encrpted form
 
 <h1>Requirments</h1>
 python3<br>
 django<br>
+cryptography,fernet Packages<br>
+Latest version of Pip<br>
+ <li>
+ to install cryptography =" pip install cryptography"
+ to install Fernet ="pip isntall fernet"
--- textutils/views.py
+++ textutils/views.py
@@ -2,6 +2,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from cryptography.fernet import Fernet
+import base64
+
 
 def index(request):
     return render(request, 'index.html')
@@ -17,6 +20,9 @@ def analyze(request):
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
     numberremover = request.POST.get('numberremover','off')
+    EncryptText = request.POST.get('EncryptText','off')
+    DecryptText = request.POST.get('DecryptText','off')
+
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -70,11 +76,39 @@ def analyze(request):
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
         djtext = analyzed
 
+    if(EncryptText=="on"):
+        analyzed =""+ encrypt(djtext)
+        params = {'purpose': 'Encrypted', 'analyzed_text': analyzed}
+        djtext = analyzed
     
-    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on"):
+    if(DecryptText=="on"):
+        analyzed =""+ decrypt(djtext)
+        params = {'purpose': 'Decrypted', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on" and EncryptText!="on" and DecryptText!="on"):
         return HttpResponse("please select any operation and try again")
 
     return render(request, 'analyze.html', params)
 
 def about(request):
     return render(request, 'about.html')
+
+key = Fernet.generate_key()
+
+def encrypt(txt):
+
+        txt = str(txt)
+        cipher_suite = Fernet(key)
+        encrypted_text = cipher_suite.encrypt(txt.encode('ascii'))
+        encrypted_text = base64.urlsafe_b64encode(encrypted_text).decode("ascii")
+        return encrypted_text
+
+
+def decrypt(txt):
+
+        txt = base64.urlsafe_b64decode(txt)
+        cipher_suite = Fernet(key)#(settings.ENCRYPT_KEY)
+        #cipher_suite = Fernet(key)
+        decoded_text = cipher_suite.decrypt(txt).decode("ascii")
+        return decoded_text 
